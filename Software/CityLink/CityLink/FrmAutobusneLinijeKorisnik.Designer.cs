f9a2f831e486f5b63238092797671ea6a48449f4
Nikola Lazar <nlazar22@student.foi.hr>
2024-05-28 13:31:27 +0200

    Male promjene vezane uz forme

--- Software/CityLink/CityLink/FrmAutobusneLinijeKorisnik.Designer.cs
+++ Software/CityLink/CityLink/FrmAutobusneLinijeKorisnik.Designer.cs
@@ -28,19 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(342, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 33);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "CityLink";
             // 
             // FrmAutobusneLinijeKorisnik
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
             this.Name = "FrmAutobusneLinijeKorisnik";
             this.Text = "Autobusne linije";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label3;
     }
 }
