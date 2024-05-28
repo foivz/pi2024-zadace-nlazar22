b219228dae9b584d184f2729d4d07fbc6696fb66
Nikola Lazar <nlazar22@student.foi.hr>
2024-05-28 12:10:02 +0200

    Provjera podataka prilikom prijave

--- Software/CityLink/CityLink/FrmLogin.Designer.cs
+++ Software/CityLink/CityLink/FrmLogin.Designer.cs
@@ -87,6 +87,7 @@
             this.btnLogin.TabIndex = 5;
             this.btnLogin.Text = "Prijava";
             this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // FrmLogin
             // 
--- Software/CityLink/CityLink/FrmLogin.cs
+++ Software/CityLink/CityLink/FrmLogin.cs
@@ -7,14 +7,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace CityLink
 {
     public partial class FrmLogin : Form
     {
+        string username1 = "zaposlenik";
+        string username2 = "korisnik";
+        string lozinka1 = "test1";
+        string lozinka2 = "test2";
         public FrmLogin()
         {
             InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "")
+            {
+                MessageBox.Show("Unesite korisničko ime!", "Pogreška",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (txtPassword.Text == "")
+            {
+                MessageBox.Show("Unesite lozinku!", "Pogreška",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                bool provjeraPrijave = false;
+
+                if (txtUsername.Text == username1 && txtPassword.Text == lozinka1)
+                {
+                    provjeraPrijave = true;
+                }
+                else if (txtUsername.Text == username2 && txtPassword.Text == lozinka2)
+                {
+                    provjeraPrijave = true;
+                }
+
+                if (provjeraPrijave)
+                {
+                    MessageBox.Show("Uspješno ste se prijavili!", "Prijavljeni ste",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Netočni podaci!", "Pogreška",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
